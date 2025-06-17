4b5759aaa4fa93f64637a2b71612a7f2c9823901
thiago_soares <thiago.soares@vs.unimed.com.br>
2025-06-16 23:37:11 -0300

    Create files to settings env

--- agents_project/wsgi.py
+++ agents_project/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agents_project.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.prod')
 
 application = get_wsgi_application()
